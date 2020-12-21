ed26ddb17369a8b203e946021f8878fb6e40094c
_3Y3 <494412189@qq.com>
2020-12-21 17:36:13 +0800

    refactor: 优化 prod 打包配置。

--- scripts/config/webpack.prod.js
+++ scripts/config/webpack.prod.js
@@ -1,12 +1,12 @@
-const { resolve } = require('path')
-const webpack = require('webpack')
-const { merge } = require('webpack-merge')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const glob = require('glob')
-const PurgeCSSPlugin = require('purgecss-webpack-plugin')
-const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
-const common = require('./webpack.common.js')
-const { PROJECT_PATH, shouldOpenAnalyzer } = require('../constants')
+const { resolve } = require('path');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const glob = require('glob');
+const PurgeCSSPlugin = require('purgecss-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const common = require('./webpack.common.js');
+const { PROJECT_PATH, shouldOpenAnalyzer } = require('../constants');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -19,12 +19,14 @@ module.exports = merge(common, {
     }),
     new webpack.BannerPlugin({
       raw: true,
-      banner: '/** @preserve Powered by react-ts-quick-starter (https://github.com/vortesnail/react-ts-quick-starter) */',
+      banner:
+        '/** @preserve Powered by react-ts-quick-starter (https://github.com/vortesnail/react-ts-quick-starter) */'
     }),
-    shouldOpenAnalyzer && new BundleAnalyzerPlugin({
-      analyzerMode: 'server',
-      analyzerHost: '127.0.0.1',
-      analyzerPort: 8888,
-    }),
-  ].filter(Boolean),
-})
+    shouldOpenAnalyzer &&
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerHost: '127.0.0.1',
+        analyzerPort: 8888
+      })
+  ].filter(Boolean)
+});
